69c7ef39a9374faa45c6a93d32755997986d691a
Sal Ceja <salceja45@gmail.com>
2023-11-22 16:18:08 -0800

    finished Readme

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -11,24 +11,16 @@ const {
   deleteReaction,
 } = require("../../controllers/thoughtController");
 
-// /api/thoughts
+// api/thoughts
 router.route("/").get(getAllthoughts).post(createThought);
-//
 router
   .route("/:thoughtId")
   .get(getSingleThought)
   .put(updateThought)
   .delete(deleteThought);
 
-// router.route("/").post(createThought);
-
-// router.route("/:thoughtId").put(updateThought);
-
-// router.route("/:thoughtId").delete(deleteThought);
-
-//Reactions:
+// Reactions:
 router.route("/:thoughtId/reactions").post(addReaction);
-
 router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
 
 module.exports = router;
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -12,25 +12,14 @@ const {
 } = require("../../controllers/userController");
 
 // /api/thoughts
-router.route("/").get(getAllUsers);
-
-router.route("/:userId").get(getSingleUser);
-
-router.route("/").post(createUser);
-
-router.route("/:userId").put(updateUser);
-
-// causing an error if this line is last bc others execute first.
-router.route("/:userId/friends/:friendId").delete(deleteFriend);
-
-router.route("/:userId").delete(deleteUser);
-
-router.route("/:userId/friends/:friendId").post(addFriend);
+router.route("/").get(getAllUsers).post(createUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
 
 /* NOTES: 
   router.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend) 
-   this is chaining. You can putl multiple HTTP methods in one single route. Doing this
+   this is chaining. If you dont chain, you might get errror with some enpoint pucking up the wrong route. Doing this
    will allow the route to handle both POST and DELETE. Meaning 'either', Post or Delete.
 */
